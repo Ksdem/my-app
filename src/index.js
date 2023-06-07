c846b3f8a8e40811d70616ad5fd9300f30d29807
Ksdem <ksdem9488@gmail.com>
2023-06-07 16:05:21 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from "/store/store-redux";
+import store from "./store/store-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
